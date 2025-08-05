787deb7770f1a4355d6b7436c906b501e7beaa07
Vahe <ohanyanv703@gmail.com>
2025-08-06 01:22:57 +0400

    footer needs additions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,59 @@
 import React from "react";
 import {} from "react-icons";
-import { FaGithub, FaTwitter,FaMailBulk } from "react-icons/fa";
+import { FaGithub, FaTwitter, FaMailBulk } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <React.Fragment>
-      <footer>
-        <div>
-          <img src="" alt="Logo" />
-          <h1>Devs Blog</h1>
-          {/* Social Links */}
-          <div>
-            <a href=""><FaGithub></FaGithub></a>
-            <a href=""><FaTwitter></FaTwitter></a>
-            <a href=""><FaMailBulk></FaMailBulk></a>
+      <footer className="text-gray-700 pt-32 px-5 ">
+        <div className="flex justify-start items-start gap-40 mb-2">
+          <div className="grid gap-1">
+
+            <Link to="/" className="flex items-center gap-2 mb-4 ">
+              <img src="" alt="Logo" />
+              <h1 className="text-2xl font-bold ">Devs Blog</h1>
+            </Link>
+
+            {/* Social Links */}
+            <div className="flex items-center justify-center space-x-5 mt-0  mb-12 text-2xl">
+              <Link to="">
+                <FaGithub></FaGithub>
+              </Link>
+              <Link to="">
+                <FaTwitter></FaTwitter>
+              </Link>
+              <Link to="">
+                <FaMailBulk></FaMailBulk>
+              </Link>
+            </div>
+          </div>
+        
+
+          <div className="grid gap-1">
+            <h3 className="text-xl font-semibold mb-2 w-30">Quick Links</h3>
+            <Link to="/" className="text-lg hover:text-purple-500 transition">Home</Link>
+            <Link to="/About" className="text-lg  hover:text-purple-500 transition">About</Link>
+            <Link to="/Contact" className="text-lg  hover:text-purple-500 transition">Contact</Link>
           </div>
 
-          <div>
-            <h3>Quick Links</h3>
-            <a href="/">Home</a>
-            <a href="/About">About</a>
-            <a href="/Contact">Contact</a>
+          <div className="grid gap-1">
+            <h3 className="text-xl font-semibold mb-2 w-30">Categories</h3>
+            <Link to="" className="text-lg hover:text-purple-500 transition">Mobile</Link>
+            <Link to="" className="text-lg hover:text-purple-500 transition">Full Stack</Link>
+            <Link to="" className="text-lg hover:text-purple-500 transition">AI & ML</Link>
+            <Link to="" className="text-lg hover:text-purple-500 transition">Backend</Link>
+            <Link to="" className="text-lg hover:text-purple-500 transition">Quality Assurance</Link>
           </div>
 
-          <div>
-            <h3>Legal</h3>
-            <a href="">Privacy Policy</a>
-            <a href="">Terms of Service</a>
-            <a href="">Cookie Policy</a>
+          <div className="grid gap-1">
+            <h3 className="text-xl font-semibold mb-2 w-30">Legal</h3>
+            <Link to="" className="text-lg hover:text-purple-500 transition">Privacy Policy</Link>
+            <Link to="" className="text-lg hover:text-purple-500 transition">Terms of Service</Link>
+            <Link to="" className="text-lg hover:text-purple-500 transition">Cookie Policy</Link>
           </div>
-
         </div>
-    <hr />
-        <div>
-            © {new Date().getFullYear()} Devs Blog. All rights reserved
-        </div>
+        <hr />
+        <div>© {new Date().getFullYear()} Devs Blog. All rights reserved</div>
       </footer>
     </React.Fragment>
   );
