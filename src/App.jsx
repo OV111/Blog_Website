02851b861c877d72683e3504bf8584cdd691266d
Vahe <ohanyanv703@gmail.com>
2025-08-06 13:15:12 +0400

    Nested Routing fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+import CategoryLayout from "./layouts/CategoriesLayout"
+
 import Home from "./pages/Home";
 import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 import FullStack from "./pages/CategoryPages/FullStack";
-import QA from "./pages/CategoryPages/QA"
+import Backend from "./pages/CategoryPages/Backend";
+import QA from "./pages/CategoryPages/QA";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route >
-          <Route path="categories/fullstack" element={<FullStack />}></Route>
-          <Route path="/qa" element={<QA />}></Route>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="signin" element={<SignIn />}></Route>
+          <Route path="signup" element={<SignUp />}></Route>
         </Route>
-        <Route path="/about" element={<About />}></Route>
+
+        <Route path="/categories" element={<CategoryLayout />}>
+          <Route path="fullstack" element={<FullStack />} />
+          <Route path="backend" element={<Backend />} />
+          <Route path="categories"></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
