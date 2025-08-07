83b5ff50058c3de2ef0d612972ef5866c2454491
Vahe <ohanyanv703@gmail.com>
2025-08-07 17:31:23 +0400

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <React.Fragment>
       {/* <div className="bg-indigo-50 min-h-screen"> bg-pink-50 */}
       <div>
-        <section className="mt-30 mb-20 ml-20 flex flex-col gap-4">
+        <section className="mt-33 mb-50 ml-20 flex flex-col gap-4">
           <div className="w-146 h-35">
             <h1 className="text-7xl text-start font-medium text-purple-800 ml-5">
               <TextType
@@ -36,7 +36,7 @@ const Home = () => {
           </GradientText>
           <div>
             {/* need to change style */}
-            <button className="items-center justify-center  ml-8 mt-4 px-10 py-2 text-2xl rounded-xl text-gray-100 border-none bg-fuchsia-700 text-whitetransition cursor-pointer hover:bg-fuchsia-600 transform hover:scale-102 hover:shadow-lg hover:text-white">
+            <button className="items-center justify-center  ml-6 mt-4 px-10 py-2 text-2xl rounded-xl text-gray-100 border-none bg-fuchsia-700 text-whitetransition cursor-pointer hover:bg-fuchsia-600 transform hover:scale-102 hover:shadow-lg hover:text-white">
               <ShinyText
                 text="Start Reading"
                 disabled={false}
