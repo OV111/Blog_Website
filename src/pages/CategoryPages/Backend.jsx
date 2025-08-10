8387dcebdecf02d49fa07fcda212572f09c1ff8f
Vahe <ohanyanv703@gmail.com>
2025-08-10 22:52:49 +0400

    Backend page added

---
+++ src/pages/CategoryPages/Backend.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+const Backend = () => {
+    return (
+        <h1>Backend Development</h1>
+    )
+}
+export default Backend;
