e1664ded04b9ed068cd48ba150e940dbe3fe1cb2
Vahe <ohanyanv703@gmail.com>
2025-08-20 00:21:31 +0400

    Auth Provider

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import { AuthContext } from "./AuthContext";
 
-const AuthProvider = () => {
-    const [isAuthenticated,setIsAuthenticated] = useState(false)
+const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    const login = (username,password) => {
-
+  const login = (username, password) => {
+    if (username && password) {
+      setIsAuthenticated(true);
     }
+    return false;
+  };
+  console.log(isAuthenticated)
 
-    const value = {isAuthenticated}
-    return (
-        <AuthContext.Provider value={value}>
-
-        </AuthContext.Provider>
-    )
-}
+  const value = { isAuthenticated, login };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
 export default AuthProvider;
