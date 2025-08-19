ef9db5e09dc6af5ebb90800680a199ab28fdee8c
Vahe <ohanyanv703@gmail.com>
2025-08-20 00:36:42 +0400

    Theme Context

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,19 +1,3 @@
-import React, { useEffect, useState } from "react";
 import { createContext } from "react";
 
-export const ContextTheme = createContext();
-
-const ThemeContext = ({ parents }) => {
-  const [theme, setTheme] = useState(() => {
-    return localStorage.getItem("theme") === "dark" ? true : false;
-  });
-
-  
-  return (
-    <ContextTheme.Provider value={(theme, setTheme)}>
-      {parents}
-    </ContextTheme.Provider>
-  );
-};
-
-export default ThemeContext;
+export const ThemeContext = createContext();
