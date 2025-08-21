2d9aa489630d84a7e8c800517f6bb5f118a7d35a
Vahe <ohanyanv703@gmail.com>
2025-08-22 00:53:55 +0400

    Home page with splash cursor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,13 @@ import TextType from "../components/TextType";
 import ShinyText from "../components/ShinyText";
 import GradientText from "../components/GradientText";
 
+import SplashCursor from "../components/SplashCursor";
+
 const Home = () => {
   return (
     <React.Fragment>
       <div>
+        <SplashCursor/>
         <section className="mt-33 mb-50 ml-20 flex flex-col gap-4 ">
           <div className="w-146 h-35">
             {/* <h1 className="text-7xl text-start font-medium text-purple-800 ml-5">
@@ -32,7 +35,6 @@ const Home = () => {
             A place to read, write and understand topics.
           </GradientText>
           <div>
-            {/* need to change style */}
             <button className="items-center justify-center  ml-6 mt-4 px-10 py-2 text-2xl rounded-xl text-gray-100 border-none bg-fuchsia-700 text-whitetransition cursor-pointer hover:bg-fuchsia-600 transform hover:scale-102 hover:shadow-lg hover:text-white dark:bg-fuchsia-550">
               <ShinyText
                 text="Start Reading"
