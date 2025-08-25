54986b45b5a6bb4ceeb43d9967d6946022570390
Vahe <ohanyanv703@gmail.com>
2025-08-25 22:09:20 +0400

    App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,48 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import React, {  Suspense } from "react";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import MainLayout from "./layouts/MainLayout";
-import CategoryLayout from "./layouts/CategoriesLayout";
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
+import AuthProvider from "./context/AuthProvider";
+import ThemeProvider from "../src/context/ThemeProvider";
 
-import FullStack from "./pages/CategoryPages/FullStack";
-import Backend from "./pages/CategoryPages/Backend";
-import Mobile from "./pages/CategoryPages/Mobile";
-import AIandML from "./pages/CategoryPages/AI&ML";
-import QA from "./pages/CategoryPages/QA";
+import router from "./router";
+import LoadingSuspense from "./components/LoadingSuspense";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="signin" element={<SignIn />}></Route>
-          <Route path="signup" element={<SignUp />}></Route>
-        </Route>
-
-        <Route path="/categories" element={<CategoryLayout />}>
-          <Route path="fullstack" element={<FullStack />} />
-          <Route path="backend" element={<Backend />} />
-          <Route path="mobile" element={<Mobile />} />
-          <Route path="ai&ml" element={<AIandML />} />
-          <Route path="qa" element={<QA />} />
-        </Route>
-
-      </Routes>
-    </BrowserRouter>
+    <React.Fragment>
+      <ThemeProvider>
+        <AuthProvider>
+          <Suspense fallback={<LoadingSuspense />}>
+            <RouterProvider router={router}></RouterProvider>
+          </Suspense>
+        </AuthProvider>
+      </ThemeProvider>
+    </React.Fragment>
   );
+  // return (
+  //   <ThemeProvider>
+  //     <AuthProvider>
+  //       <BrowserRouter>
+  //         <Routes>
+  //           <Route element={<MainLayout />}>
+  //             <Route path="/" element={<Home />}></Route>
+  //             <Route path="about" element={<About />}></Route>
+  //             <Route path="sign-in" element={<SignIn />}></Route>
+  //             <Route path="sign-up" element={<SignUp />}></Route>
+  //           </Route>
+
+  //           <Route path="/categories" element={<CategoryLayout />}>
+  //             <Route path="fullstack" element={<FullStack />} />
+  //             <Route path="backend" element={<Backend />} />
+  //             <Route path="mobile" element={<Mobile />} />
+  //             <Route path="ai&ml" element={<AIandML />} />
+  //             <Route path="qa" element={<QA />} />
+  //           </Route>
+  //         </Routes>
+  //       </BrowserRouter>
+  //     </AuthProvider>
+  //   </ThemeProvider>
+  // );
 };
 export default App;
