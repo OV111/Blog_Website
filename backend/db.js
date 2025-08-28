dadadd5d9fdf85c330a5376eafe62fecdb073100
Vahe <ohanyanv703@gmail.com>
2025-08-28 15:29:53 +0400

    DataBase file that connect to mongodb

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,23 @@
-import { MongoClient } from "mongodb"
-import dotenv from "dotenv"
-dotenv.config()
-const MONGO_URI = process.env.MONGO_URI
-const client = new MongoClient(MONGO_URI)
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+const client = new MongoClient(MONGO_URI);
+
+let db;
 
 const connectDB = async () => {
-    try{
-        await client.connect();
-        console.log("MongoDB connected successfully!");
-        return client.db("DevsBlog")
-    } catch(err) {
-        console.error("MongoDB connection error",err);
-        return null
-    }
+  if (db) return db; // reuse existing connection
+
+  try {
+    await client.connect();
+    console.log("MongoDB connected successfully!");
+    db = client.db("DevsBlog"); // store reference to DB
+    return db;
+  } catch (err) {
+    console.error("MongoDB connection error", err);
+    throw err;
+  }
 };
 export default connectDB;
