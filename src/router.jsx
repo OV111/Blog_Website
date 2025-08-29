03dd218b8fc6ac039118011f5f256e1d10ab5bed
Vahe <ohanyanv703@gmail.com>
2025-08-30 00:50:23 +0400

    new Route for LogOut

--- src/router.jsx
+++ src/router.jsx
@@ -5,17 +5,16 @@ import MainLayout from "./layouts/MainLayout";
 import CategoryLayout from "./layouts/CategoriesLayout";
 import NotFound from "./components/NotFound";
 
+import LogOut from "./pages/LogOut";
 const Home = lazy(() => import("./pages/Home"));
 const About = lazy(() => import("./pages/About"));
-const SignIn = lazy(() => import("./pages/SignIn"));
-const SignUp = lazy(() => import("./pages/SignUp"));
+const GetStarted = lazy(() => import("./pages/GetStarted"));
 
 const FullStack = lazy(() => import("./pages/CategoryPages/FullStack"));
 const Backend = lazy(() => import("./pages/CategoryPages/Backend"));
 const Mobile = lazy(() => import("./pages/CategoryPages/Mobile"));
 const AIandML = lazy(() => import("./pages/CategoryPages/AI&ML"));
 const QA = lazy(() => import("./pages/CategoryPages/QA"));
-
 
 const router = createBrowserRouter([
   {
@@ -24,8 +23,8 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <Home /> },
       { path: "about", element: <About /> },
-      { path: "sign-in", element: <SignIn /> },
-      { path: "sign-up", element: <SignUp /> },
+      { path: "get-started", element: <GetStarted /> },
+      {path: "log-out", element: <LogOut/>}
     ],
   },
   {
