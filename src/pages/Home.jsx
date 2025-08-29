25cc4b8b6e641da3e658b3d415ec62e86b66ec16
Vahe <ohanyanv703@gmail.com>
2025-08-29 21:24:39 +0400

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import TextType from "../components/TextType";
 import ShinyText from "../components/ShinyText";
 import GradientText from "../components/GradientText";
@@ -12,19 +13,19 @@ const Home = () => {
         <SplashCursor/>
         <section className="mt-33 mb-50 ml-20 flex flex-col gap-4 ">
           <div className="w-146 h-35">
-            {/* <h1 className="text-7xl text-start font-medium text-purple-800 ml-5">
+            <h1 className="text-7xl text-start font-medium text-purple-800 ml-5">
               <TextType
                 text={[
                   "Welcome to Developers blog.",
                   "Level up your dev skills,",
                   "Created by Devs for Devs!",
                 ]}
-                typingSpeed={105}
-                pauseDuration={1500}
+                typingSpeed={109}
+                pauseDuration={1700}
                 showCursor={true}
                 cursorCharacter="|"
               />
-            </h1> */}
+            </h1>
           </div>
           <GradientText
             colors={["#8A2BE2", "#FF1493", "#FF00FF", "#9c40ff", "#00FF00"]}
@@ -35,13 +36,14 @@ const Home = () => {
             A place to read, write and understand topics.
           </GradientText>
           <div>
-            <button className="items-center justify-center  ml-6 mt-4 px-10 py-2 text-2xl rounded-xl text-gray-100 border-none bg-fuchsia-700 text-whitetransition cursor-pointer hover:bg-fuchsia-600 transform hover:scale-102 hover:shadow-lg hover:text-white dark:bg-fuchsia-550">
-              <ShinyText
+            <button className="items-center justify-center  ml-6 mt-4 px-10 py-2 text-2xl rounded-xl text-gray-100 border-none bg-fuchsia-700 text-whitetransition cursor-pointer hover:bg-fuchsia-600 transform  hover:scale-102 hover:shadow-lg hover:text-white dark:bg-fuchsia-550">
+              <Link to="get-started">Start Reading</Link>  
+              {/* <ShinyText
                 text="Start Reading"
                 disabled={false}
                 speed={3}
                 className="text-white font-semibold"
-              />
+              /> */}
             </button>
           </div>
         </section>
