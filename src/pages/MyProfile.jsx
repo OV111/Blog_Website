f5f61cf51135b65852365f85ed70a6c3f725f415
Vahe <ohanyanv703@gmail.com>
2025-09-01 22:48:33 +0400

    My profile page

---
+++ src/pages/MyProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MyProfile = () => {
+
+    return (
+        <React.Fragment>
+            <p>My Profile dashboard</p>
+            <h1>Adding here Delete Account from Database</h1>
+        </React.Fragment>
+    )
+}
+export default MyProfile;
