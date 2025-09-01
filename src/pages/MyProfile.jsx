21417cda69076d10a84191a1d12678c4da18834b
Vahe <ohanyanv703@gmail.com>
2025-09-01 22:58:25 +0400

    Log Out page content in Delete Account which gonna be in myprofile under list

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import DeleteAccount from "../pages/DeleteAccount"
 const MyProfile = () => {
 
     return (
         <React.Fragment>
             <p>My Profile dashboard</p>
             <h1>Adding here Delete Account from Database</h1>
+            <DeleteAccount></DeleteAccount>
         </React.Fragment>
     )
 }
