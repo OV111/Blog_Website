dab3e942420c15f8d54912aa7331500a82f1d83d
Vahe <ohanyanv703@gmail.com>
2025-09-03 17:08:49 +0400

    changed theme with zustand

--- src/context/useThemeStore.js
+++ src/context/useThemeStore.js
@@ -2,11 +2,15 @@ import { create } from "zustand";
 
 const useThemeStore = create((set, get) => ({
   theme: localStorage.getItem("theme") || "light",
-  setTheme: () => {
-    const newTheme = get().theme === "dark" ? "light" : "dark";
-    set({ theme: newTheme });
-    localStorage.setItem("theme", newTheme);
-  },
+//   setTheme: () => {
+//     const newTheme = get().theme === "dark" ? "light" : "dark";
+//     set({ theme: newTheme });
+//     localStorage.setItem("theme", newTheme);
+//   },
+  setTheme : () => {
+    set({theme: get().theme === "dark" ? "light" : "dark"})
+    localStorage.setItem("theme",get().theme)
+  }
 }));
 
 export default useThemeStore;
