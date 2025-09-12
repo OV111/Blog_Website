d66d31b6c7b18cf613f8298635bd604e38299ab4
Vahe <ohanyanv703@gmail.com>
2025-09-12 16:50:11 +0400

    For full stack and backned pages handling default posts

--- src/pages/CategoryPages/Backend.jsx
+++ src/pages/CategoryPages/Backend.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
+
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
 const Backend = () => {
-    return (
-        <h1>Backend Development</h1>
-    )
-}
+  const fetchingPosts = async () => {
+    const request = await fetch(`${VITE_API_BASE_URL}/categories/backend`, {
+        method: "GET",
+        headers:{"Content-Type" : "application/json"}
+    }); 
+
+    const response = await request.json();
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchingPosts();
+  }, []);
+  return <h1>Backend Development</h1>;
+};
 export default Backend;
--- src/pages/CategoryPages/FullStack.jsx
+++ src/pages/CategoryPages/FullStack.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { postData } from "../../data/posts";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -9,18 +8,35 @@ import Typography from "@mui/material/Typography";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
 
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
 const FullStack = () => {
-  const filteredPosts = postData.filter(
-    (post) => post.category === "fullstack"
-  );
+
+
+
+const fetchPosts = async () => {
+  const request = await fetch(`${VITE_API_BASE_URL}/categories/fullstack`, {
+      method: "GET",
+      headers: {"content-type" : "application/json"},
+      credentials: 'include'
+    })
+
+    const response = await request.json()
+    console.log(response)
+  }
+
+  useEffect(() => {
+    fetchPosts()
+    
+  },[])
 
   return (
     <React.Fragment>
       <div >
         <h1 className="flex items-center justify-center text-3xl py-4 font-medium text-sky-800">Full Stack Development Posts</h1>
-        {console.log(filteredPosts)}
+        
         <Grid container spacing={4} padding={5}>
-          {filteredPosts.map((post) => (
+          {[].map((post) => (
             <Card sx={{ minWidth: 425, maxWidth:425}} variant="outlined">
               <CardMedia sx={{ height: 200 }} image={post.image} />
               <CardContent>
