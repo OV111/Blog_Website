07635709d66de951b6d0395278067ce66d8e0c14
Vahe <ohanyanv703@gmail.com>
2025-09-14 00:36:23 +0400

    FullStack page with default posts from db

--- src/pages/CategoryPages/FullStack.jsx
+++ src/pages/CategoryPages/FullStack.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
@@ -8,62 +8,73 @@ import Typography from "@mui/material/Typography";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
 
-const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+const LoadingSuspense = lazy(() => import("../../components/LoadingSuspense"));
+
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const FullStack = () => {
-
-
-
-const fetchPosts = async () => {
-  const request = await fetch(`${VITE_API_BASE_URL}/categories/fullstack`, {
-      method: "GET",
-      headers: {"content-type" : "application/json"},
-      credentials: 'include'
-    })
-
-    const response = await request.json()
-    console.log(response)
-  }
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchPosts()
-    
-  },[])
+    const fetchPosts = async () => {
+      try {
+        const request = await fetch(
+          `${VITE_API_BASE_URL}/categories/fullstack`,
+          {
+            method: "GET",
+            headers: { "content-type": "application/json" },
+            credentials: "include",
+          }
+        );
+
+        const response = await request.json();
+        setData(response);
+        setLoading(!loading);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchPosts();
+  }, []);
 
   return (
     <React.Fragment>
-      <div >
-        <h1 className="flex items-center justify-center text-3xl py-4 font-medium text-sky-800">Full Stack Development Posts</h1>
-        
-        <Grid container spacing={4} padding={5}>
-          {[].map((post) => (
-            <Card sx={{ minWidth: 425, maxWidth:425}} variant="outlined">
-              <CardMedia sx={{ height: 200 }} image={post.image} />
-              <CardContent>
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  alignItems={"center"}
-                  textAlign={"center"}
-                >
-                  {post.title}
-                </Typography>
-                <Typography variant="body2" maxHeight={400}>
-                  {post.description}
-                </Typography>
-              </CardContent>
-              <CardActions >  
-                <Button
-                size="small"
-                component={Link}
-                to={``}
-                >
-                    Read More 
-                </Button>
-              </CardActions>
-            </Card>
-          ))}
-        </Grid>
+      <h1 className="flex justify-center items-center text-xl font-medium text-sky-800 py-6 sm:text-2xl md:text-4xl lg:text-5xl">
+        Full Stack Development Posts
+      </h1>
+      <div>
+        <Suspense fallback={<LoadingSuspense></LoadingSuspense>}>
+          {loading ? (
+            <LoadingSuspense></LoadingSuspense>
+          ) : (
+            <Grid container spacing={4} padding={5}>
+              {data.map((post) => (
+                <Card sx={{ minWidth: 425, maxWidth: 425 }} variant="outlined">
+                  <CardMedia sx={{ height: 200 }} image={post.image} />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      alignItems={"center"}
+                      textAlign={"center"}
+                    >
+                      {post.title}
+                    </Typography>
+                    <Typography variant="body2" maxHeight={400}>
+                      {post.description}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" component={Link} to={``}>
+                      Read More
+                    </Button>
+                  </CardActions>
+                </Card>
+              ))}
+            </Grid>
+          )}
+        </Suspense>
       </div>
     </React.Fragment>
   );
