2bca0649c5e2e26ff5d8c16d8685674c86013674
Vahe <ohanyanv703@gmail.com>
2025-09-14 20:44:31 +0400

    Devops page update

--- src/pages/CategoryPages/DevOps.jsx
+++ src/pages/CategoryPages/DevOps.jsx
@@ -1,24 +1,71 @@
-import React,{lazy,Suspense,useEffect,useState} from 'react'
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import {Link} from "react-router-dom"
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+
+const LoadingSuspense = lazy(() => import("../../components/LoadingSuspense"));
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const DevOps = () => {
-    const [loading,setLoading] = useState(true)
-    useEffect(() => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-    },[])
+  useEffect(() => {
+    const fetchingPosts = async () => {
+      const request = await fetch(`${VITE_API_BASE_URL}/categories/devops`, {
+        method: "GET",
+        headers: { "content-type": "application/json" },
+      });
+      const response = await request.json();
 
-    return (
-        <React.Fragment>
-            <h1>DevopS Posts</h1>
-            <div>
-                <Suspense fallback={}>
-        {loading?  (
-            <></>
-        ) : (
-            
-        )}
-                </Suspense>
-            </div>
-        </React.Fragment>
-    )
-}
+      setData(response);
+      setLoading(false);
+    };
+    fetchingPosts();
+  },[]);
+
+  return (
+    <React.Fragment>
+      <h1 className="">DevOps Posts</h1>
+      <div>
+        <Suspense fallback={<LoadingSuspense></LoadingSuspense>}>
+          {loading ? (
+            <LoadingSuspense />
+          ) : (
+            <Grid container spacing={4} padding={5}>
+              {data.map((post) => (
+                <Card sx={{ minWidth: 425, maxWidth: 425 }} variant="outlined">
+                  <CardMedia sx={{ height: 200 }} image={post.image} />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      alignItems={"center"}
+                      textAlign={"center"}
+                    >
+                      {post.title}
+                    </Typography>
+                    <Typography variant="body2" maxHeight={400}>
+                      {post.description}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" component={Link} to={``}>
+                      Read More
+                    </Button>
+                  </CardActions>
+                </Card>
+              ))}
+            </Grid>
+          )}
+        </Suspense>
+      </div>
+    </React.Fragment>
+  );
+};
 export default DevOps;
