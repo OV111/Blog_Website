3b72afe137eaabaa06a94231e5a56601f16dc2fc
Vahe <ohanyanv703@gmail.com>
2025-09-20 19:11:11 +0400

    Privacy page

---
+++ src/pages/Privacy.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Privacy = () => {
+  return (
+    <React.Fragment>
+      <h1>Privacy</h1>
+      <p>Research for privacy of websites</p>
+    </React.Fragment>
+  );
+};
+export default Privacy;
