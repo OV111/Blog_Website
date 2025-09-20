9529ab03578c1bd103a1772dc3a1d4e2cbea1299
Vahe <ohanyanv703@gmail.com>
2025-09-20 17:06:33 +0400

    settings

--- src/pages/My-Profile/Settings.jsx
+++ src/pages/My-Profile/Settings.jsx
@@ -1,5 +1,12 @@
+import React from 'react'
 const Settings = () => {
-
+    console.log("vaeh")
+    return (
+        <React.Fragment>
+            <p>Settings</p>
+        </React.Fragment>
+    )
 
 }
-export default Settings
+
+export default Settings;
