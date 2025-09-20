798320f3ba93a855b151573b68dbc5f456980d9d
Vahe <ohanyanv703@gmail.com>
2025-09-20 19:10:09 +0400

    zustand store

--- src/context/useAuthStore.js
+++ src/context/useAuthStore.js
@@ -1,9 +1,15 @@
 import { create } from "zustand";
 
 const useAuthStore = create((set) => ({
-  auth: false,
-  login: () => set({ auth: true }),
-  logout: () => set({ auth: false }),
+  auth: JSON.parse(localStorage.getItem("AuthToken")) || false,
+  login: () => {
+    localStorage.setItem("AuthToken", true);
+    set({ auth: true });
+  },
+  logout: () => {
+    localStorage.setItem("AuthToken", false);
+    set({ auth: false });
+  },
 }));
 
-export  default useAuthStore;
+export default useAuthStore;
