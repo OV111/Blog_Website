52e95da0c7e8937cc32ab92ad0c4fd3af92806af
Vahe <ohanyanv703@gmail.com>
2025-09-21 19:15:59 +0400

    Settiing default html form

--- src/pages/My-Profile/Settings.jsx
+++ src/pages/My-Profile/Settings.jsx
@@ -3,7 +3,24 @@ const Settings = () => {
     console.log("vaeh")
     return (
         <React.Fragment>
-            <p>Settings</p>
+            <div>
+            <h1>General</h1>
+            <div className='grid gap-2'>
+
+            <label htmlFor="">f,l Name</label>
+            <input type="text" placeholder='username' className='max-w-100 border-1 ' />
+
+            <label htmlFor="">Bio</label>
+            <input type="text" placeholder='bio' className='max-w-100 border-1 ' />
+
+            <label htmlFor="">Links of Github</label>
+            <input type="text"  placeholder='Enter New Url' className='max-w-100 border-1 '/>
+            <label htmlFor="">Links of Linkedin</label>
+            <input type="text" placeholder='Enter New Url' className='max-w-100 border-1 '/>
+            <label htmlFor="">Links of Twitter</label>
+            <input type="text" placeholder='Enter New Url'className='max-w-100 border-1 '/>
+            </div>
+            </div>
         </React.Fragment>
     )
 
