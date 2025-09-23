ce2c293b38350431909875cad15354e0f02a4e59
Vahe <ohanyanv703@gmail.com>
2025-09-23 16:09:16 +0400

    Setting page addes images changes

--- src/pages/My-Profile/Settings.jsx
+++ src/pages/My-Profile/Settings.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import DeleteAccount from "../DeleteAccount";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const Settings = () => {
   const [fname, setFname] = useState("");
   const [lname, setLname] = useState("");
+  const [profileImage, setProfileImage] = useState(null);
+  const [bannerImage, setBannerImage] = useState(null);
   const [bio, setBio] = useState("");
   const [postsCount, setPostsCount] = useState(0);
   const [githubLink, setGithubLink] = useState("");
@@ -55,7 +58,7 @@ const Settings = () => {
       });
       const response = await request.json();
       console.log(response);
-      console.log(response.message)
+      console.log(response.message);
       if (!request.ok) {
         setFname(oldProfile.fname);
         setLname(oldProfile.lname);
@@ -64,8 +67,8 @@ const Settings = () => {
         setLname(response.user.lastName);
         setBio(response.stats.bio);
         setGithubLink(response.stats.githubLink);
-        setLinkedinLink(response.stats.linkedinLink)
-        setTwitterLink(response.stats.twitterLink)
+        setLinkedinLink(response.stats.linkedinLink);
+        setTwitterLink(response.stats.twitterLink);
       }
     } catch (err) {
       console.log(err);
@@ -81,46 +84,58 @@ const Settings = () => {
             type="text"
             placeholder="username"
             className="max-w-100 border-1 "
-             onChange={(e) => setFname(e.target.value)}
+            onChange={(e) => setFname(e.target.value)}
           />
           <label htmlFor="">LName</label>
           <input
             type="text"
             placeholder="username"
             className="max-w-100 border-1 "
-             onChange={(e) => setLname(e.target.value)}
+            onChange={(e) => setLname(e.target.value)}
           />
 
+          <label htmlFor="">Profile Image</label>
+          <input
+            type="image"
+            placeholder="username"
+            className="max-w-100 border-1 "
+            onChange={() => {}}
+          />
+          <label htmlFor="">Banner Image</label>
+          <input
+            type="image"
+            placeholder="username"
+            className="max-w-100 border-1 "
+            onChange={() => {}}
+          />
           <label htmlFor="">Bio</label>
           <input
             type="text"
             placeholder="bio"
             className="max-w-100 border-1 "
-             onChange={(e) => setBio(e.target.value)}
+            onChange={(e) => setBio(e.target.value)}
           />
 
-    <label htmlFor="">Last Active</label>
-          <label htmlFor="">Posts Count</label>
           <label htmlFor="">Links of Github</label>
           <input
             type="text"
             placeholder="Enter New Url"
             className="max-w-100 border-1 "
-             onChange={(e) => setGithubLink(e.target.value)}
+            onChange={(e) => setGithubLink(e.target.value)}
           />
           <label htmlFor="">Links of Linkedin</label>
           <input
             type="text"
             placeholder="Enter New Url"
             className="max-w-100 border-1 "
-             onChange={(e) => setLinkedinLink(e.target.value)}
+            onChange={(e) => setLinkedinLink(e.target.value)}
           />
           <label htmlFor="">Links of Twitter</label>
           <input
             type="text"
             placeholder="Enter New Url"
             className="max-w-100 border-1 "
-             onChange={(e) => setTwitterLink(e.target.value)}
+            onChange={(e) => setTwitterLink(e.target.value)}
           />
 
           <button
@@ -133,6 +148,8 @@ const Settings = () => {
           </button>
         </div>
       </div>
+
+      <DeleteAccount />
     </React.Fragment>
   );
 };
