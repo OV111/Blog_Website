bcb1d9edfb1db2bc6bc76ad3989ea19c3b704b4a
Vahe <ohanyanv703@gmail.com>
2025-09-25 23:55:56 +0400

    Add Blog page created fetch

--- src/pages/My-Profile/AddBlog.jsx
+++ src/pages/My-Profile/AddBlog.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const AddBlog = () => {
   const [title, setTitle] = useState();
@@ -9,7 +10,13 @@ const AddBlog = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const request = 0;
+      const request = await fetch(`${API_BASE_URL}/blogs`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({title,text,category,file}),
+      });
+      const response = await request.json();
+      console.log(response.message,response.code);
     } catch (err) {
       console.log(err);
     }
@@ -18,17 +25,17 @@ const AddBlog = () => {
   return (
     <React.Fragment>
       <p>Add Blog Post</p>
-      <form className="grid " onSubmit={() => handleSubmit()}>
-        <input type="text" placeholder="Enter Title" />
+      <form className="grid " onSubmit={handleSubmit}>
+        <input type="text" value={title} placeholder="Enter Title" onChange={(e) => {setTitle(e.target.value)}}/>
         <textarea
           placeholder="Write your blog..."
-          //   value={}
-          onChange={(e) => e.target.value}
+            value={text}
+          onChange={(e) => setText(e.target.value)}
           required
           rows={6}
           className="border p-2 rounded"
         />
-        <select value={category}>
+        <select value={category} onChange={(e)=>{setCategory(e.target.value)}}>
           <option value="">Select Category</option>
           <option value="FullStack">FullStack</option>
           <option value="Backend">Backend</option>
@@ -37,7 +44,7 @@ const AddBlog = () => {
           <option value="Quality Assurance">Quality Assurance</option>
           <option value="DevOps">DevOps</option>
         </select>
-        <input type="file" className="border p-2 rounded-md" />
+        <input type="file" value={file} onChange={(e) => {setFile(e.target.value)}} className="border p-2 rounded-md" />
         <button className="border p-2 rounded-md cursor-pointer ">
           Sumbit{" "}
         </button>
