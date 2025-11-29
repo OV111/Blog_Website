367908010ef30dea19ceb3448b77790a40f63db0
Vahe <ohanyanv703@gmail.com>
2025-11-29 21:42:20 +0400

    handle default Posts

---
+++ backend/routes/handleDefPosts.js
@@ -0,0 +1,29 @@
+// import settingDefaultPosts from "../seeders/posts-default.js";
+import connectDB from "../config/db.js";
+
+export const defaultPostsHandling = async (category) => {
+// ! here I need to cache into Redis then use for 
+// ! each request(avoiding fetching every time from DB)
+// console.log(auth)
+  try {
+    let db = await connectDB();
+
+    const collectionPostsDefault = db.collection("posts-default");
+
+    let posts = await collectionPostsDefault.find({category:category}).toArray()
+
+    return {
+      status:200,
+      data: posts,
+      message:"Posts Successfully loaded",
+    }
+  } catch (err) {
+    console.log(err);
+    return {
+      status: 500,
+      message: "Server Error!",
+    }
+  }
+};
+
+// defaultPostsHandling
