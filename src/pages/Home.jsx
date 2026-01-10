cc6d73cf3e16488033786f54beaa68da8cd66ffb
Vahe <ohanyanv703@gmail.com>
2026-01-11 00:16:19 +0400

    Home page added componont visual

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,16 +35,16 @@ const Home = () => {
   // useEffect(() => {
   //   if (!document.cookie.includes("cookiesAccepted=true")) {
   //     toast(
-  //       <div className="grid mx-auto gap-3">
-  //         <h1 className="text-xl font-bold text-center pt-2">
+  //       <div className="gap-3 grid mx-auto">
+  //         <h1 className="pt-2 font-bold text-xl text-center">
   //           We use Cookies 🍪
   //         </h1>
-  //         <p className="text-lg  text-gray-600 font-medium">
+  //         <p className="font-medium text-gray-600 text-lg">
   //           We use cookies to enhance your experience. By continuing to visit
   //           this site, you agree to our use of cookies.
   //         </p>
   //         <button
-  //           className="bg-purple-600 w-full rounded-xl py-2 text-white  cursor-pointer hover:bg-fuchsia-600 transform  hover:scale-102 hover:text-white"
+  //           className="bg-purple-600 hover:bg-fuchsia-600 py-2 rounded-xl w-full text-white hover:text-white hover:scale-102 cursor-pointer transform"
   //           onClick={() => {
   //             acceptCookies()
   //           }}
@@ -61,7 +61,7 @@ const Home = () => {
 
   return (
     <React.Fragment>
-      <div className="lg:min-h-screen">
+      <div className="flex gap-30 lg:min-h-screen">
         <div>
           <Toaster
             position="right-bottom"
@@ -72,9 +72,9 @@ const Home = () => {
             {/* <SplashCursor/> */}
           </Suspense>
           {/* 94 for width*/}
-          <section className="mt-[100px] mb-[350px] ml-8 flex flex-col gap-2 sm:gap-8 lg:gap-10 max-w-4xl sm:ml-[40px] md:ml-[60px] lg:ml-[80px] ">
-            <div className="max-w-43 h-20 lg:max-w-143 lg:h-35 ">
-              <h1 className="text-xl font-medium ml-2 text-purple-800 lg:text-5xl sm:text-5xl md:text-6xl  lg:text-7xl lg:pl-4 lg:ml-0 dark:text-purple-600">
+          <section className="flex flex-col gap-2 sm:gap-8 lg:gap-10 mt-[100px] mb-[350px] ml-8 sm:ml-[40px] md:ml-[60px] lg:ml-[80px] max-w-4xl">
+            <div className="max-w-43 lg:max-w-143 h-20 lg:h-35">
+              <h1 className="ml-2 lg:ml-0 lg:pl-4 font-medium text-purple-800 dark:text-purple-600 text-xl sm:text-5xl lg:text-5xl md:text-6xl lg:text-7xl">
                 <TextType
                   text={[
                     "Welcome to Developers blog.",
@@ -93,23 +93,30 @@ const Home = () => {
               colors={["#8A2BE2", "#FF1493", "#FF00FF", "#9c40ff", "#00FF00"]}
               animationSpeed={8}
               showBorder={false}
-              className="text-start w-[300px] font-medium ml-2 text-xl mt-4 py-1 px-1 sm:text-xl md:text-2xl md:w-[200] lg:text-3xl lg:w-[100%]"
+              className="mt-4 ml-2 px-1 py-1 w-[300px] md:w-[200] lg:w-[100%] font-medium text-xl sm:text-xl md:text-2xl lg:text-3xl text-start"
             >
               A place to read, write and understand topics.
             </GradientText>
             {/* </div> */}
             <div>
-              <button className="items-center justify-center font-medium ml-3 mt-2 px-6 py-1 text-lg rounded-xl text-gray-100 border-none bg-fuchsia-700 text-whitetransition cursor-pointer hover:bg-fuchsia-600 transform  hover:scale-102 hover:shadow-lg hover:text-white dark:bg-fuchsia-550 lg:px-10 lg:py-2 lg:text-2xl lg:mt-2">
+              <button className="justify-center items-center bg-fuchsia-700 hover:bg-fuchsia-600 dark:bg-fuchsia-550 hover:shadow-lg mt-2 lg:mt-2 ml-3 px-6 lg:px-10 py-1 lg:py-2 border-none rounded-xl font-medium text-gray-100 text-whitetransition hover:text-white text-lg lg:text-2xl hover:scale-102 cursor-pointer transform">
                 <Link to="get-started">Start Reading</Link>
                 {/* <ShinyText
                 text="Start Reading"
                 disabled={false}
                 speed={3}
-                className="text-white font-semibold"
+                className="font-semibold text-white"
               /> */}
               </button>
             </div>
           </section>
+        </div>
+        <div>
+          <img
+            src="src/assets/componentImage.png"
+            alt="image"
+            className="w-120 h-120"
+          />
         </div>
       </div>
     </React.Fragment>
