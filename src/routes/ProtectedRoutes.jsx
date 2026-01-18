4088a703b3d5c6e67706a3c00b94bbe74f563579
Vahe <ohanyanv703@gmail.com>
2026-01-18 21:40:58 +0400

    Fixes for initial load of myProfile page

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -3,8 +3,10 @@ import { Navigate } from "react-router-dom";
 import useAuthStore from "../context/useAuthStore";
 
 const ProtectedRoutes = ({children}) => {
+  const token = localStorage.getItem("JWT");
+
   const { auth } = useAuthStore();
-    if(!auth) {
+    if(auth || token) {
         return <Navigate to='/get-started' />
     }
     return children
