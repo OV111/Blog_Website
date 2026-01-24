130d5b91102e485ef8fd48f786c6465a2ce6a019
Vahe <ohanyanv703@gmail.com>
2026-01-24 14:43:27 +0400

    main Layout height fix

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,9 +4,10 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 const MainLayout = () => {
   return (
+    // bg-zinc-100
     <div className="bg-zinc-100 dark:bg-gray-900 min-h-screen">
       <Navbar />
-      <main className="flex-grow">
+      <main className="min-h-screen">
         <Outlet />
       </main>
       <Footer />
